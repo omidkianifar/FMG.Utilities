e40b0107ab7f9cd669418deeb1f0bc8c44dbd17d
IAMOK\Omid <omid.kianifar@gmail.com>
2023-02-25 04:44:16 +0330

    no message

---
+++ Runtime/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fmg.Utilities
+{
+    public static class StringExtensions
+    {
+        public const string Full = "";
+    }
+}
---
+++ Tests/Runtime/StringExtensionsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Fmg.Utilities.Tests.Runtime
+{
+    public class StringExtensionsTest
+    {
+        public void TestFull() 
+        {
+            Debug.Log(StringExtensions.Full);
+        }
+    }
+}
